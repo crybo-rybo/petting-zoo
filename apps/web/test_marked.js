76b9d50e57a9676956e553a792fe2863e5cc454d
Conor <rybackiconor@gmail.com>
2026-02-22 10:47:24 -0500

    Delete Test File From Unnecessary Place

--- apps/web/test_marked.js
+++
@@ -1,9 +0,0 @@
-const { marked } = require('marked');
-const renderer = {
-  code(token) {
-    console.log("Token properties:", Object.keys(token));
-    return `<pre><code>${token.text}</code></pre>`;
-  }
-};
-marked.use({ renderer });
-console.log(marked.parse('```python\nprint("hello")\n```'));
